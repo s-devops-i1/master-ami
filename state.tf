e6b38b996648d2e12fa058b03361c59a358cb3ba
shuja <shuja.hafez@gmail.com>
2024-07-04 12:49:24 +0530

    name of pipeline fixed

--- state.tf
+++ state.tf
@@ -1,4 +1,7 @@
-bucket = "b78-sbucket"
-key    = "test2/dev/sfile"
-region = "us-east-1"
-
+terraform {
+  backend "s3" {
+    bucket = "b78-sbucket"
+    key    = "test2/dev/sfile"
+    region = "us-east-1"
+  }
+}
